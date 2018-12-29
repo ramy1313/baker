adfb72fbef327d7229345994a486e875c1c52606
Rami <rmohamed.ext@orange.com>
2018-12-30 00:20:25 +0200

    mixed packs

--- test.js
+++ test.js
@@ -26,3 +26,11 @@ it("Order 14 Blueberry Muffin should return 3 packs of 2 and 1 pack of 8", () =>
     assert.equal(orderDetails.items.some(i => i.packSize === 2 && i.price === 9.95 && i.multiple === 3), true)
     assert.equal(orderDetails.items.some(i => i.packSize === 8 && i.price === 24.95 &&  i.multiple === 1), true)
 })
+
+it("Order 13 Croissant should return 2 packs of 5 and 1 pack of 3", () => {
+    const orderDetails = bakerOrder.order(13, "CF")
+    assert.equal(orderDetails.totalPrice, 25.85)
+    assert.equal(orderDetails.items.length, 2)
+    assert.equal(orderDetails.items.some(i => i.packSize === 5 && i.price === 9.95 && i.multiple === 2), true)
+    assert.equal(orderDetails.items.some(i => i.packSize === 1 && i.price === 5.95 &&  i.multiple === 1), true)
+})
